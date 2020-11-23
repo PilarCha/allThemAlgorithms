5b46aa3dd2e149cf080b2c7e7db7c01c9f972278
PilarCha <pchavez92@gmail.com>
2020-11-23 12:55:58 -0800

    Implementing BST boilerplate

---
+++ Implementation/ImplementingBST.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(data);
+  remove(data);
+
+  // Helper Function
+  findMinNode() {
+
+  }
+
+  getRootNode() {
+    
+  }
+
+  inorder(node) {
+
+  }
+
+  preorder(node) {
+
+  }
+
+  postorder(node) {
+
+  }
+
+  search(node, data) {
+
+  }
+}
