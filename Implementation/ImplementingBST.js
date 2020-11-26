94100037f12532637274f19d0a5101f52f4f8899
PilarCha <pchavez92@gmail.com>
2020-11-25 16:43:08 -0800

    Finished the BST Insert helper function

--- Implementation/ImplementingBST.js
+++ Implementation/ImplementingBST.js
@@ -11,8 +11,32 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(data);
-  remove(data);
+  insert(data) {
+    var newNode = new Node(data);
+
+    if (this.root === null)
+      this.root = newNode;
+    else
+      this.insertNode(this.root,newNode);
+
+    insertNode(node, newNode) {
+      if(newNode.data < node.data) {
+        if(node.left === null)
+          node.left = newNode;
+        else
+          this.insertNode(node.left, newNode);
+      } else {
+        if(node.right === null)
+          node.right = newNode;
+        else
+          this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  remove(data) {
+
+  }
 
   // Helper Function
   findMinNode() {
@@ -20,7 +44,7 @@ class BinarySearchTree {
   }
 
   getRootNode() {
-    
+
   }
 
   inorder(node) {
