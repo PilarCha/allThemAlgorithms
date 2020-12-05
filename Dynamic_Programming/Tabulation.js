faee78c198438ff6bc8d334e0840cf07e55c65b8
PilarCha <pchavez92@gmail.com>
2020-12-04 22:22:47 -0800

    Final commit b4 sleep

--- Dynamic_Programming/Tabulation.js
+++ Dynamic_Programming/Tabulation.js
@@ -1,6 +1,11 @@
-const fib = (n) => {
+//memoization
+// js object, keys will be arg to fn, value will be the return value
+
+const fib = (n,memo = {}) => {
+  if(n in memo) return memo[n];
   if(n <= 2) return 1;
-  return fib(n-1) + fib(n-2);
+  memo[n] = fib(n-1,memo) + fib(n-2,memo);
+  return memo[n];
 }
 
 console.log(fib(6));
@@ -9,5 +14,5 @@ console.log(fib(13));
 //gets stuck here
 console.log(fib(80));
 
-//time is 0(n);
+//time is 0(2n); bottleneck
 //space is 0(n);
